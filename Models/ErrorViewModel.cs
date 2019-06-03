002ad0439526c3c7037c0bc15c54bacd33db24eb
CHRISTIAN DELA BAJAN <delabac@ph.ibm.com>
2019-06-04 02:40:11 +0800

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace companyABC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
